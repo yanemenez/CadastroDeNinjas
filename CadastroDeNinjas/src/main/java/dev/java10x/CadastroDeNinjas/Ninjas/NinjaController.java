298f8e018c3bb08725346e8c5f18f10692f8008e
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-03 14:06:54 -0300

    feat: altera a rota do controller para listar os ninjas e injeta a deoendência do SERVICE

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -0,0 +1,48 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ninjas")
+public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/boasVindas")
+    public String BoasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
+    }
+
+    @GetMapping("/listarID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar Ninja por ID";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por ID";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja Deletado por ID";
+    }
+
+}
+
---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -0,0 +1,21 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
+
+
+}
