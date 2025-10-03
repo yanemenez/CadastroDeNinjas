f9ae0eb03df9f5a121dab4ac527020169c1d1153
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-03 13:59:55 -0300

    feat(ninja): adiciona NinjaRepository estendendo JpaRepository

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
