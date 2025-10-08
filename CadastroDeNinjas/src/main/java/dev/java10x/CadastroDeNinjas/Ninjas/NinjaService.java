266febfee1c4bc0c2f3804448c45a3d42cbca809
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-08 14:47:47 -0300

    feat(ninja): adiciona metodos de atualização e exclusão no NinjaService

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -28,5 +28,16 @@ public class NinjaService {
         return ninjaRepository.save(ninja);
     }
 
+    public void deletarNinjaPorId (Long id) {
+        ninjaRepository.deleteById(id);
+    }
+
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado) {
+        if (ninjaRepository.existsById(id)) {
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
 
 }
