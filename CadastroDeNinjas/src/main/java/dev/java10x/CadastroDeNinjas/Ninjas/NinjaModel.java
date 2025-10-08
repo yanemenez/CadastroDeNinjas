416c5faaaf8c329013b5e0b97840b6639507a8a3
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-08 20:23:40 -0300

    feat(ninja): adiciona campo rank ao NinjaModel

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -29,6 +29,9 @@ public class NinjaModel {
     @Column(name = "img_url")
     private String imgUrl;
 
+    @Column(name = "rank")
+    private String rank;
+
     @Column(name = "idade")
     private int idade;
 
