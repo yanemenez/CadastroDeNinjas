844fc5fc9a203c9ca1d8ae06582fa593d2ddcce5
Loyane Menezes <loyanesezenem5@gmail.com>
2025-10-27 22:00:37 -0300

    refactor(ninja): adiciona modificador 'final' nas dependências NinjaService, NinjaRepository e NinMapper

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RequestMapping("/ninjas")
 public class NinjaController {
 
-    private NinjaService ninjaService;
+    private final NinjaService ninjaService;
 
     public NinjaController(NinjaService ninjaService) {
         this.ninjaService = ninjaService;
